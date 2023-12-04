3da857482236cd35f97914509f007b2a50d3387d
lufangzhou <lufangzhou@gmail.com>
2023-12-04 19:30:17 +0800

    deploy

---
+++ assets/index.210281ab.js
@@ -0,0 +1,1 @@
+import{d as e,r as _,o,a as s,a4 as t}from"./index.bcb2642e.js";const n={class:"prop-demo"},a=e({__name:"index",setup(d){return _(),(c,p)=>(o(),s("div",n," 3 "))}});const i=t(a,[["__scopeId","data-v-d210127d"]]);export{i as default};
